896465e02819e5adf4ec22ee2cd47c898203e2d9
Ettore <utitytools@gmail.com>
2026-04-03 23:07:32 +0200

    chore: clean Vite boilerplate, setup base project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 function App() {
   return (
-    <h1 className="text-4xl font-bold text-blue-500">
-      Tailwind funcionando ✅
-    </h1>
+    <main>
+      <h1>Hello Portfolio</h1>
+    </main>
   )
 }
 
